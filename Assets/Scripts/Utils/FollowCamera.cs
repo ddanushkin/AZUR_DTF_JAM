e81cb7a432e9dcd4f05a807ecae24091d3f6b74a
RomanPay <42111208+RomanPay@users.noreply.github.com>
2020-07-18 17:01:20 +0300

    Follow camera add

---
+++ Assets/Scripts/Utils/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothTime = 0.1f;
+    
+    private void LateUpdate()
+    {
+        Vector3 dir = target.GetComponent<Rigidbody2D>().velocity;
+        Vector3 toMove = new Vector3(target.position.x + (1f * dir.x), target.position.y + (0.5f * dir.y), transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, toMove, smoothTime * Time.deltaTime);
+    }
+
+}
