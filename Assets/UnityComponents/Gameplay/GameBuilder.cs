7f8e9143f6027df99fb3bd06b396d654255e4e6e
ddanushkin <ddanushkin@gmail.com>
2020-07-18 20:25:13 +0300

    Meta update

---
+++ Assets/UnityComponents/Gameplay/GameBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameBuilder : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabStartRoom;
+    [SerializeField] private GameObject prefabPlayer;
+    [SerializeField] private GameObject[] prefabsFloors = new GameObject[3];
+    [SerializeField] private GameObject circle;
+     
+    private void Awake()
+    {
+        //Instantiate(prefabPlayer, Vector3.zero, Quaternion.identity);
+    }
+
+    void Start()
+    {
+        // Instantiate(prefabStartRoom, new Vector3(0, 0, 0), Quaternion.identity);
+        
+        // for (int i = 1; i < 5; i++)
+        // {
+            // Instantiate(prefabsFloors[Random.Range(0, 3)], new Vector3(0, i * 2, 0), Quaternion.identity);
+        // }
+    }
+}
