82625844dae5f81f5b212436001c2d41d86e4172
RomanPay <42111208+RomanPay@users.noreply.github.com>
2020-07-18 18:06:28 +0300

    Level Builder && refactor scene

--- Assets/Scripts/Utils/FollowCamera.cs
+++ Assets/Scripts/Utils/FollowCamera.cs
@@ -6,14 +6,26 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    public Transform target;
+    private Transform _target;
 
     public float smoothTime = 0.1f;
-    
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
+    {
+    }
+
+    private void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        _rigidbody2D = _target.GetComponent<Rigidbody2D>();
+    }
+
     private void LateUpdate()
     {
-        Vector3 dir = target.GetComponent<Rigidbody2D>().velocity;
-        Vector3 toMove = new Vector3(target.position.x + (1f * dir.x), target.position.y + (0.5f * dir.y), transform.position.z);
+        Vector3 dir = _rigidbody2D.velocity;
+        Vector3 toMove = new Vector3(_target.position.x + (1f * dir.x), _target.position.y + (0.5f * dir.y), transform.position.z);
         transform.position = Vector3.Lerp(transform.position, toMove, smoothTime * Time.deltaTime);
     }
 
